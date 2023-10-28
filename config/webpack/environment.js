1bfbad9bb20900563caed3a9489de7049e8212db
Blake Knight <blake.knight@zapier.com>
2023-10-27 20:54:03 -0500

    :construction: WIP to find why plyr isn't working

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,17 @@
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const { environment } = require("@rails/webpacker");
+const { VueLoaderPlugin } = require("vue-loader");
+const vue = require("./loaders/vue");
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-module.exports = environment
+const crypto = require("crypto");
+
+// HACK: Fix Node.js 17+ "digital envelope routines::initialization error"
+// https://stackoverflow.com/q/69692842/1130172
+const origCreateHash = crypto.createHash;
+crypto.createHash = (algorithm, options) => {
+  const newAlgorithm = algorithm === "md4" ? "md5" : algorithm;
+  return origCreateHash(newAlgorithm, options);
+};
+
+environment.plugins.prepend("VueLoaderPlugin", new VueLoaderPlugin());
+environment.loaders.prepend("vue", vue);
+module.exports = environment;
